1dd3d1e6eca263306318b98947d049a101d13b51
Mona Eid <104376815+Monaeid2001@users.noreply.github.com>
2024-02-26 06:43:57 +0200

    updates to side-bar component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AdminComponent } from './pages/admin/admin.component';
 import { AuthorListComponent } from './components/author/author-list/author-list.component';
-
+import {SideBarComponent} from './pages/home/side-bar/side-bar.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { BookListComponent } from './components/book/book-list/book-list.component';
 import { BookDetailsComponent } from './components/book/book-details/book-details.component';
 import { CategoryListComponent } from './components/category/category-list/category-list.component';
 import { ReviewsComponent } from './components/book/reviews/reviews.component';
 import { AuthorDetailsComponent } from './components/author/author-details/author-details.component';
-import { CategoryBooksComponent } from './components/category/category-books/category-books.component';
 export const routes: Routes = [
 
     {
@@ -26,44 +25,44 @@ export const routes: Routes = [
         path:"books",
         component:BookListComponent,
         title:"Books",
-      
+
     },
     {
         path:"books/:id",
         component:BookDetailsComponent,
         title:"Book Details",
-      
+
     },
     {
         path:"categories",
         component:CategoryListComponent,
         title:"Categories",
-      
-    },
-    {
-        path:"categories/:id",
-        component:CategoryBooksComponent,
-        title:"Category Books",
-      
+
     },
     {
         path:"authors",
         component:AuthorListComponent,
         title:"Authors",
-      
+
     },
     {
         path:"authors/:id",
         component:AuthorDetailsComponent,
         title:"Author Details",
-      
+
     },
     {
         path:"review",
         component:ReviewsComponent,
         title:"reviews",
-      
+
     },
+    {
+      path:"user",
+      component:SideBarComponent,
+      title:"user home",
+
+  },
       {
           path:"**",
           component:NotfoundComponent,
