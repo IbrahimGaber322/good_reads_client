72b9cbd5eeed71308b6ed633e35ee3707df7deb3
ahmed x <ahmedkhaled4219@gmail.com>
2024-02-29 23:00:30 +0200

    books,authors pagination

--- src/app/components/author/author-list/author-list.component.ts
+++ src/app/components/author/author-list/author-list.component.ts
@@ -2,22 +2,46 @@ import { Component } from '@angular/core';
 import { AuthorCardComponent } from '../author-card/author-card.component';
 import { AuthorService } from '../../../services/author/author.service';
 import Author from '../../../interfaces/author';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { PagingConfig } from '../../../interfaces/paging-config';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-author-list',
   standalone: true,
-  imports: [AuthorCardComponent],
+  imports: [AuthorCardComponent,NgxPaginationModule,NgFor],
   templateUrl: './author-list.component.html',
   styleUrl: './author-list.component.css',
 })
 export class AuthorListComponent {
   authors!: Author[];
+  authorCount: number = 0;
 
+  pagingConfig: PagingConfig = {
+    itemsPerPage: 5,
+    currentPage: 1,
+    totalItems: 0,
+  };
   constructor(private authorService: AuthorService) {}
 
   ngOnInit() {
-    this.authorService
-      .getAuthors()
-      .subscribe((res) => (this.authors = res.authors));
+    this.fetchauthors();
+
   }
+  fetchauthors(page: number = 1, limit: number = 5) {
+    this.authorService.getAuthors(page, limit).subscribe((data) => {
+      this.authors = data.authors;
+      this.authorCount = data.authorsCount;
+      this.pagingConfig = {
+        itemsPerPage: limit,
+        currentPage: page,
+        totalItems: this.authorCount,
+      };
+    });
+  }
+  onTableDataChange(page: number) {
+    this.pagingConfig.currentPage = page;
+    this.fetchauthors(page, this.pagingConfig.itemsPerPage);
+  }
+
 }
--- src/app/components/book/book-list/book-list.component.ts
+++ src/app/components/book/book-list/book-list.component.ts
@@ -4,21 +4,45 @@ import { BookService } from '../../../services/book/book.service';
 import { Book } from '../../../interfaces/book';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../../navbar/navbar.component';
+import { PagingConfig } from '../../../interfaces/paging-config';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { NgFor } from '@angular/common';
 
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [BookCardComponent],
+  imports: [BookCardComponent,NgxPaginationModule,NgFor],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css',
 })
 export class BookListComponent {
   books!: Book[];
+  bookCount: number = 0;
 
+  pagingConfig: PagingConfig = {
+    itemsPerPage: 5,
+    currentPage: 1,
+    totalItems: 0,
+  };
   constructor(private bookService: BookService) {}
 
   ngOnInit() {
-    this.bookService.getAllBooks().subscribe((res) => (this.books = res.books));
+    this.fetchbooks();
+  }
+  fetchbooks(page: number = 1, limit: number = 5) {
+    this.bookService.getAllBooks(page, limit).subscribe((data) => {
+      this.books = data.books;
+      this.bookCount = data.booksCount;
+      this.pagingConfig = {
+        itemsPerPage: limit,
+        currentPage: page,
+        totalItems: this.bookCount,
+      };
+    });
+  }
+  onTableDataChange(page: number) {
+    this.pagingConfig.currentPage = page;
+    this.fetchbooks(page, this.pagingConfig.itemsPerPage);
   }
 }
