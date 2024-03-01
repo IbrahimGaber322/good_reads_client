d05bd909940569cb776aa7f4531a03c3de82b40c
Ibrahim Gaber <ibrahimseda322@gmail.com>
2024-03-01 11:50:23 +0200

    user guard

--- src/app/guards/user.guard.ts
+++ src/app/guards/user.guard.ts
@@ -7,6 +7,8 @@ import {
 } from '@angular/router';
 import { TokenService } from '../services/token/token.service';
 import { UserService } from '../services/user/user.service';
+import { Observable, of } from 'rxjs';
+import { switchMap, catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -18,19 +20,30 @@ export class UserGuard implements CanActivate {
     private router: Router
   ) {}
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): any {
-    this.tokenService.authToken$.subscribe((token) => {
-      if (!token) {
-        return true;
-      }
-      this.userService.getUser(token).subscribe((user) => {
-        if (user) {
-          return false;
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    return this.tokenService.authToken$.pipe(
+      switchMap((token) => {
+        if (!token) {
+          return of(true);
         } else {
-          return true;
+          return this.userService.getUser(token).pipe(
+            map((user) => {
+              if (user) {
+                this.router.navigate(['/']);
+                return false;
+              } else {
+                return true;
+              }
+            }),
+            catchError(() => {
+              return of(true); // handle error, for example, if user is not found
+            })
+          );
         }
-      });
-      return true;
-    });
+      }),
+      catchError(() => {
+        return of(true); // handle error, for example, if token is not available
+      })
+    );
   }
 }
